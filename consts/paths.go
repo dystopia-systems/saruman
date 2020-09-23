9c5d1fce3fb105181e0627fc8326b473378b4b42
Boris Lachev <boris.lachevbg00@gmail.com>
2020-09-24 01:47:31 +0300

    update routes

---
+++ consts/paths.go
@@ -0,0 +1,5 @@
+package consts
+
+const GOSNIFF_CONFIG_PATH = "pub/gosniff.json"
+const EINSTEIN_CONFIG_PATH = "pub/einstein.json"
+const SARUMAN_CONFIG_PATH = "pub/saruman.json"
---
+++ consts/routes.go
@@ -0,0 +1,5 @@
+package consts
+
+const RedirectUrl = "https://dystopia.systems"
+const IndexUrl = "/"
+const ConfigUrl = "/config/{app-id}"
