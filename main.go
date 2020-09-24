1a10eb856630d48a721d0d8872685bcab78aa00e
Boris Lachev <boris.lachevbg00@gmail.com>
2020-09-24 03:19:33 +0300

    update launch

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
-package saruman
+package main
 
-import "github.com/vectorman1/saruman/serve"
+import (
+	"github.com/vectorman1/alaskalog"
+	"github.com/vectorman1/saruman/src/core/db"
+	"github.com/vectorman1/saruman/src/web/routes"
+	"github.com/vectorman1/saruman/src/web/serve"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main(){
-	err := serve.HandleRequests()
+	routes.InitializeMap()
 
-	if err != nil {
-		panic(err)
+	r := serve.SetupRoutes()
+	dbErr := db.InitDb()
+
+	if dbErr != nil {
+		alaskalog.Logger.Fatalf("Failed to init db. %v", dbErr)
+		return
 	}
+
+	migrationErr := db.InitMigration()
+
+	if migrationErr != nil {
+		alaskalog.Logger.Fatalf("Failed to migrate db. %v", dbErr)
+		return
+	}
+
+	alaskalog.Logger.Fatal(http.ListenAndServe(":3000", r))
+
+	alaskalog.Logger.Infoln("Relay is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
 }
