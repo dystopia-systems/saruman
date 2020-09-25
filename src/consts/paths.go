da9459692ec2c6cd53fd4deeba42cd0a71b4a6ec
Boris Lachev <boris.lachevbg00@gmail.com>
2020-09-25 08:21:57 -0700

    update paths

--- src/consts/paths.go
+++ src/consts/paths.go
@@ -1,5 +1,3 @@
 package consts
 
-const GOSNIFF_CONFIG_PATH = "pub/gosniff.json"
-const EINSTEIN_CONFIG_PATH = "pub/einstein.json"
-const SARUMAN_CONFIG_PATH = "pub/saruman.json"
+const CONFIG_STORE_PATH="STORE/CONFIG"
